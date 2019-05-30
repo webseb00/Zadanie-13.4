1053bda773cef1b0f4eff9699517bdb04ae220cd
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-05-30 22:40:40 +0200

    Create basic files

---
+++ modules/getOSinfo.js
@@ -0,0 +1,28 @@
+var os = require('os');
+var time = require('./formatTime');
+time.print();
+
+function getOSinfo() {
+
+    var type = os.type();
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if (type === 'Windows_NT') {
+        type = 'Windows';
+    }
+
+    console.log('System: ', type);
+    console.log('Release: ', release);
+    console.log('CPU: ', cpu);
+    console.log('Uptime: ', formatTime(uptime));
+    console.log('User name: ', userInfo.username);
+    console.log('User homedir: ', userInfo.homedir);
+
+}
+
+exports.print = getOSinfo;
